eda89ef32ef9ee91a31bf22cf23d443db83800a2
Evandroverdino <evandroverdino@gmail.com>
2018-09-17 13:16:25 -0300

    Add new username and token to run metrics collector

--- src/main/java/utils/Config.java
+++ src/main/java/utils/Config.java
@@ -11,14 +11,16 @@ public class Config {
 	//public static final String USERNAME = "danieltmo";
 	//public static final String PASSWORD = "ef0b74ea7fc3f19f3f1bfeaf0126f4ba363b9371";
 	
-	public static final String USERNAME = "filipefalcaos";
-	public static final String PASSWORD = "7433ebeb886a0edcde0f82136608fef703383f28";
+	//public static final String USERNAME = "filipefalcaos";
+	//public static final String PASSWORD = "7433ebeb886a0edcde0f82136608fef703383f28";
 
 	//public static final String USERNAME = "francamacdowell";
 	//public static final String PASSWORD = "60b21522f8d7e4eb47e0814de0a1a438140001c8";
 	
 	//public static final String USERNAME = "Nicholas-ar";
 	//public static final String PASSWORD = "11df8aadc315f66fca18614d2335bc14907f6f9a";
-
 	
+	public static final String USERNAME = "Evandroverdino";
+	public static final String PASSWORD = "498d88c9b22ff8960105573dbcfd7a6472214163";
+
 }
